0e64b93d2d7f114ed98fb90de28478d15039a557
Faruk Albayrak <faruk_albayrak@outlook.com>
2016-12-17 02:17:02 +0300

    Dosyalar Yüklendi

---
+++ Assets/Scripts/MenuKod.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MenuKod : MonoBehaviour {
+
+	// Use this for initialization
+    public void LoadScreen(string screenName)
+    {
+        SceneManager.LoadScene(screenName);
+    }
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
